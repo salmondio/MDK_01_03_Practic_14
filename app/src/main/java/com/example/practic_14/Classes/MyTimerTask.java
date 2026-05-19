c3a73299fa60ab88a2f5c3e48a9f8637fcb7e9d0
salmondio <sergej.serzanin1@gmail.com>
2026-05-19 20:12:56 +0500

    разработан класс для таймера приложения сержанини

---
+++ app/src/main/java/com/example/practic_14/Classes/MyTimerTask.java
@@ -0,0 +1,38 @@
+package com.example.practic_14.Classes;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.TimerTask;
+
+public class MyTimerTask extends TimerTask {
+    public int Time = 30;
+    public Activity Activity;
+    public TextView tvText, tvSendMail;
+    public MyTimerTask(Activity activity, TextView tvText, TextView tvSendMail){
+        this.Activity = activity;
+        this.tvText = tvText;
+        this.tvSendMail = tvSendMail;
+    }
+
+    @Override
+    public void run(){
+        Time--;
+        if(Time == 0)
+            this.cancel();
+
+        Activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String Second = Time > 10 ? String.valueOf(Time) : "0" + String.valueOf(Time);
+                tvText.setText("00:" + Second);
+
+                if(Time == 0){
+                    tvText.setVisibility(View.GONE);
+                    tvSendMail.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+}
